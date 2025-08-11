ccb486f02d9b980cb97d38b962488821e2797459
ravenkim <41793830+ravenkim@users.noreply.github.com>
2025-08-11 17:47:41 +0900

    동적 url 규칙 변경

--- src/app/router/router.tsx
+++ src/app/router/router.tsx
@@ -1,35 +1,41 @@
-import { createBrowserRouter, RouteObject } from 'react-router'
-import HomePage from 'src/pages/HomePage'
-import React from 'react'
-import NotFoundPage from 'src/pages/extra/NotFoundPage.tsx'
+import { createBrowserRouter, RouteObject } from 'react-router';
+import HomePage from 'src/pages/HomePage';
+import React from 'react';
+import NotFoundPage from 'src/pages/extra/NotFoundPage.tsx';
 
-// NOTE: https://reactrouter.com/start/data/routing
-// TODO: lazy loading 적용해야 할까? > 필요 없을거 같음
-
-const MODULES = import.meta.glob('src/pages/url/**/*.tsx', {
-    eager: true,
-}) as Record<string, { default: React.FC }>
+const MODULES = import.meta.glob(
+    ['src/pages/url/**/*.tsx'],
+    {
+        eager: true,
+    }
+) as Record<string, { default: React.FC }>;
 
 const generateRoutes = (
     modules: Record<string, { default: React.FC }>,
 ): RouteObject[] => {
-    return Object.entries(modules).map(([path, module]) => {
+    const routes: RouteObject[] = [];
+
+    Object.entries(modules).forEach(([path, module]) => {
         // 파일 경로에서 'src/pages/url/' 이후의 경로를 추출
-        const routePath = path
-            .replace(/.*src\/pages\/url\//, '') // 'src/pages/url/' 부분 제거
-            .replace(/\.tsx$/, '') // 확장자 제거
-            .replace(/Page$/, '') // 'Page' 접미사 제거
-            .replace(/\[(.*?)]/g, ':$1') // [param] -> :param 변환
-            .toLowerCase()
+        const relativePath = path.replace(/.*src\/pages\/url\//, '');
+        const Component = module.default;
 
-        const Component = module.default
+        // 파일명(index.tsx, [id].tsx 등)을 제외하고 폴더명만 추출
+        const urlPath = relativePath
+            .replace(/\/[^/]*\.tsx$/, '') // 마지막 슬래시와 파일명 제거
+            .replace(/\[(.*?)]/g, ':$1'); // 동적 라우팅 파라미터 처리
 
-        return {
-            path: `/${routePath}`,
+        // 동적 라우팅이 아닌 경우를 위해 기본 경로('/')를 처리
+        const finalUrlPath = urlPath === '' ? '/' : `/${urlPath}`;
+
+        routes.push({
+            path: finalUrlPath,
             element: <Component />,
-        }
-    })
-}
+        });
+    });
+
+    return routes;
+};
 
 const router = createBrowserRouter([
     {
@@ -43,6 +49,6 @@ const router = createBrowserRouter([
         path: '*',
         element: <NotFoundPage />,
     },
-])
+]);
 
-export default router
+export default router;
