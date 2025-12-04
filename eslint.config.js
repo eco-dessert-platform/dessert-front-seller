4eeea40d211746fa9d0125d12f3b28d3b4db7181
hyeone999 <caps026@naver.com>
2025-12-04 10:22:22 +0900

    chore: unused-vars rule 수정

--- eslint.config.js
+++ eslint.config.js
@@ -23,7 +23,12 @@ export default tseslint.config(
                 'warn',
                 { allowConstantExport: true },
             ],
-            '@typescript-eslint/no-unused-vars': ['warn'],
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                {
+                    argsIgnorePattern: '^_',
+                },
+            ],
         },
     },
 )
