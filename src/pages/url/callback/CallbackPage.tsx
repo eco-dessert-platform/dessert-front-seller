b7c6672b62e0bfda9e6b1bcc4eb7bf32b34bd2b7
hyeone999 <caps026@naver.com>
2025-12-04 10:24:40 +0900

    Feat: 소셜로그인 콜백 구현

---
+++ src/pages/url/callback/CallbackPage.tsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react'
+import { useParams, useSearchParams } from 'react-router-dom'
+
+const CallbackPage = () => {
+    const { provider } = useParams<{ provider: string }>()
+    const [searchParams] = useSearchParams()
+
+    useEffect(() => {
+        const code = searchParams.get('code')
+        const error = searchParams.get('error')
+
+        if (error) {
+            console.error('소셜 로그인 에러:', error)
+            if (window.opener) {
+                window.opener.postMessage(
+                    { type: 'SOCIAL_LOGIN_ERROR', error },
+                    window.location.origin,
+                )
+            }
+            window.close()
+            return
+        }
+
+        if (code) {
+            if (window.opener) {
+                window.opener.postMessage(
+                    { type: 'SOCIAL_LOGIN_SUCCESS', provider, code },
+                    window.location.origin,
+                )
+            }
+            window.close()
+        }
+    }, [searchParams, provider])
+
+    return
+}
+
+export default CallbackPage
