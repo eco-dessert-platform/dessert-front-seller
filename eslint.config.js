42c52017d123a231e65c43a88efad9b722e997ad
김해준 <ravenkim97@onpoom.co.kr>
2025-12-05 12:00:53 +0900

    안쓰는 LINT 추가

--- eslint.config.js
+++ eslint.config.js
@@ -25,9 +25,7 @@ export default tseslint.config(
             ],
             '@typescript-eslint/no-unused-vars': [
                 'warn',
-                {
-                    argsIgnorePattern: '^_',
-                },
+            
             ],
         },
     },
