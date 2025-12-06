510b2c532b7a3923116264a80914543f5eabc485
crud0626 <sisofiy6048@gmail.com>
2025-12-06 17:50:56 +0900

    feat: orderTable 액션 버튼 disabled 속성 추가

--- src/features/orders/components/OrderControlButtons.tsx
+++ src/features/orders/components/OrderControlButtons.tsx
@@ -16,50 +16,51 @@ type ActionType =
 type ButtonConfig = {
     label: string
     action: ActionType
+    disabled: boolean
 }
 
 const BUTTONS_BY_TAB = {
     ALL: [],
     PAID: [
-        { label: '발주확인', action: 'ORDER_CONFIRMED' },
-        { label: '주문취소', action: 'CANCEL_APPROVED' },
+        { label: '발주확인', action: 'ORDER_CONFIRMED', disabled: false },
+        { label: '주문취소', action: 'CANCEL_APPROVED', disabled: false },
     ],
     CHECKED: [
-        { label: '주문취소', action: 'CANCEL_APPROVED' },
-        { label: '반품', action: 'RETURN_APPROVED' },
+        { label: '주문취소', action: 'CANCEL_APPROVED', disabled: false },
+        { label: '반품', action: 'RETURN_APPROVED', disabled: false },
     ],
     SHIPPED: [
-        { label: '반품', action: 'RETURN_APPROVED' },
-        { label: '교환', action: 'EXCHANGE_APPROVED' },
+        { label: '반품', action: 'RETURN_APPROVED', disabled: false },
+        { label: '교환', action: 'EXCHANGE_APPROVED', disabled: false },
     ],
     DELIVERED: [
-        { label: '반품', action: 'RETURN_APPROVED' },
-        { label: '교환', action: 'EXCHANGE_APPROVED' },
+        { label: '반품', action: 'RETURN_APPROVED', disabled: false },
+        { label: '교환', action: 'EXCHANGE_APPROVED', disabled: false },
     ],
     PAYMENT_COMPLETED: [
-        { label: '취소승인', action: 'CANCEL_APPROVED' },
-        { label: '취소거절', action: 'CANCEL_REJECTED' },
+        { label: '취소승인', action: 'CANCEL_APPROVED', disabled: false },
+        { label: '취소거절', action: 'CANCEL_REJECTED', disabled: false },
     ],
     REFUND: [
         [
-            { label: '반품승인', action: 'RETURN_APPROVED' },
-            { label: '반품거절', action: 'RETURN_REJECTED' },
+            { label: '반품승인', action: 'RETURN_APPROVED', disabled: false },
+            { label: '반품거절', action: 'RETURN_REJECTED', disabled: false },
         ],
         [
-            { label: '반품완료', action: 'RETURN_COMPLETED' },
-            { label: '반품반려', action: 'RETURN_RETURNED' },
-            { label: '반품보류', action: 'RETURN_ON_HOLD' },
+            { label: '반품완료', action: 'RETURN_COMPLETED', disabled: true },
+            { label: '반품반려', action: 'RETURN_RETURNED', disabled: true },
+            { label: '반품보류', action: 'RETURN_ON_HOLD', disabled: true },
         ],
     ],
     CHANGE: [
         [
-            { label: '반품승인', action: 'RETURN_APPROVED' },
-            { label: '반품거절', action: 'RETURN_REJECTED' },
+            { label: '반품승인', action: 'RETURN_APPROVED', disabled: false },
+            { label: '반품거절', action: 'RETURN_REJECTED', disabled: false },
         ],
         [
-            { label: '반품완료', action: 'RETURN_COMPLETED' },
-            { label: '반품반려', action: 'RETURN_RETURNED' },
-            { label: '반품보류', action: 'RETURN_ON_HOLD' },
+            { label: '반품완료', action: 'RETURN_COMPLETED', disabled: true },
+            { label: '반품반려', action: 'RETURN_RETURNED', disabled: true },
+            { label: '반품보류', action: 'RETURN_ON_HOLD', disabled: true },
         ],
     ],
 } satisfies Record<TabCategory, ButtonConfig[] | ButtonConfig[][]>
@@ -94,6 +95,7 @@ const OrderControlButtons = ({
                             <Button
                                 key={buttonIndex}
                                 onClick={() => onClick(button.action)}
+                                disabled={button.disabled}
                                 className="text-12 h-[30px] min-w-[61px] border border-gray-200 bg-white p-0 font-medium"
                             >
                                 {button.label}
@@ -112,6 +114,7 @@ const OrderControlButtons = ({
                     key={buttonIndex}
                     variant="outline"
                     onClick={() => onClick(button.action)}
+                    disabled={button.disabled}
                     className="text-12 h-[30px] min-w-[61px] border border-gray-200 p-0"
                 >
                     {button.label}
